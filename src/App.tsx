29c74e68ab4f69b5374d5f058924c8ed7b0b39e8
SlavaHeorh <slava.georg.k@gmail.com>
2023-05-04 13:52:03 +0300

    work with axios, added custom hook and some components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import {Product} from "./components/Product";
-import {products} from "./data/products";
+import {useProducts} from "./hooks/products";
+import {Loader} from "./components/Loader";
+import {ErrorMessage} from "./components/ErrorMessage";
 
 function App() {
+
+    const {products, error, loading} = useProducts()
+
+
     return (
         <div className="container mx-auto max-w-2xl pt-5">
+            {loading && <Loader/>}
+            {error && <ErrorMessage error={error}/>}
+            {/*создаем новый массив с продуктами*/}
             {products.map(product => <Product product={product} key={product.id}/>)}
         </div>
     )
